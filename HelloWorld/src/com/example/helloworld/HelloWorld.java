5ac1ff55fe522e736db53f52fa8f83cf5eb4be57
IsabellaGuinto <isabella.guinto@benilde.edu.ph>
2025-10-17 15:59:01 +0800

    Added Hello World

---
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+ public static void main(String[] args){
+     //System.out.println("Hello World");
+
+     Scanner scanner = new Scanner(System.in);
+
+     System.out.println("What is your name?");
+     String name = scanner.nextLine();
+
+     System.out.println("Hello " + name);
+ }
+}
